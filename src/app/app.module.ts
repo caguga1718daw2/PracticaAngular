8126a933a5124ed3fbc63b4c084a930d65a7faea
Carlos Guimovart <carlosguimovart@MacBook-MacBook-Pro-de-Carlos.local>
2018-12-18 18:53:06 +0100

    commit creació de taula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
+import { FavoritosComponent } from './favoritos/favoritos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaulaPelisComponent } from './taula-pelis/taula-pelis.component';
+import { TaulaPelisComponent } from './NetMovies/taula-pelis/taula-pelis.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaulaPelisComponent
+    TaulaPelisComponent,
+    FavoritosComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
